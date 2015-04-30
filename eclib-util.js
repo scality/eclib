b9098ebe0cd9ef7aec6fa38da44fa21f0fffa852
Shafiul tareq <shafiul.hasan@widespace.com>
2015-04-30 11:06:13 +0600

    Utill filed added
    eclib-util.js will provide the necessary methods for the main npm module

---
+++ eclib-util.js
@@ -0,0 +1,11 @@
+// All the util method will go here.
+// Lets see how many methods do we need add here, might be in the future we goona need 
+//		another validator util
+
+function ECLibUtil(){
+
+	console.log("ECLibUtil function");
+}
+
+
+module.exports = ECLibUtil;
