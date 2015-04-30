28db75b0cf4c2a83e3b25549d93d9d826e73631b
Shafiul tareq <shafiul.hasan@widespace.com>
2015-04-30 12:24:30 +0600

    Just checking the public methods of ECLib
    Just initialize the ECLib and checking all the methods outputs in console.

--- test/temptest.js
+++ test/temptest.js
@@ -2,8 +2,31 @@
 // Will remove the file when we can communicate with the real libarary 
 // and can add the mocha test files.
 
+var ECLib = require('../node-eclib.js');
 
 (function() {
-	console.log("From the temp test file");
-	
+
+	console.log("JS method existense testing");
+
+	var eclib = new ECLib();
+
+	eclib.create();
+	eclib.destroy();
+	eclib.encode();
+	eclib.encodeCleanup();
+	eclib.decode();
+	eclib.decodeCleanup();
+	eclib.reconstructFragment(); 
+	eclib.fragmentsNeeded();
+
+	eclib.getFragmentMetadata();
+	eclib.isInvalidFragment();
+	eclib.verifyStripeMetadata();
+	eclib.getAlignedDataSize();
+	eclib.getMinimumEncodeSize();
+	eclib.getFragmentSize();
+
+	console.log("ALL the methods are OK.");
+
+
 })();
