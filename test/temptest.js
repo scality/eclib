20507f24bfb0b4b967664d855020da3bbed0a808
Shafiul tareq <shafiul.hasan@widespace.com>
2015-04-30 12:09:19 +0600

    Temporary testing file added to the repo. Will remove it after the mocha integration

---
+++ test/temptest.js
@@ -0,0 +1,9 @@
+// Just trying to do the manual checking of the method existenance 
+// Will remove the file when we can communicate with the real libarary 
+// and can add the mocha test files.
+
+
+(function() {
+	console.log("From the temp test file");
+	
+})();
