afa8a9a4a4b41169bb80181a86470d3c44654bb8
Shafiul tareq <shafiul.hasan@widespace.com>
2015-05-01 12:18:37 +0600

    All the methods are exposed to node addon.
    All the public methods of liberasurecode are  now exposed to node addons. All methods are
    now returning only a static message containing  method name.

--- src/cpp/libmain.cpp
+++ src/cpp/libmain.cpp
@@ -1,19 +1,56 @@
 #include <node.h>
 #include <nan.h>
+#include <erasurecode.h>
+#include <erasurecode_helpers.h>
 
 #include "libmain.h"
 
 
-NAN_METHOD(Create) {
+NAN_METHOD(EclCreate) {
   NanScope();
-
-
   NanReturnValue(NanNew("C++ create "));
 }
 
-NAN_METHOD(Destroy) {
+NAN_METHOD(EclDestroy) {
   NanScope();
-
-
   NanReturnValue(NanNew("C++ destroy "));
 }
+
+
+NAN_METHOD(EclFragmentsNeeded) {
+  NanScope();
+  NanReturnValue(NanNew("C++ FragmentsNeeded "));
+}
+
+
+NAN_METHOD(EclGetFragmentMetadata) {
+  NanScope();
+  NanReturnValue(NanNew("C++ GetFragmentMetadata "));
+}
+
+NAN_METHOD(EclIsInvalidFragment) {
+  NanScope();
+  NanReturnValue(NanNew("C++ IsInvalidFragment "));
+}
+
+NAN_METHOD(EclVerifyStripeMetadata) {
+  NanScope();
+  NanReturnValue(NanNew("C++ VerifyStripeMetadata "));
+}
+
+NAN_METHOD(EclGetAlignedDataSize) {
+  NanScope();
+  NanReturnValue(NanNew("C++ GetAlignedDataSize "));
+}
+
+NAN_METHOD(EclGetMinimumEncodeSize) {
+  NanScope();
+  NanReturnValue(NanNew("C++ GetMinimumEncodeSize "));
+}
+
+NAN_METHOD(EclGetFragmentSize) {
+  NanScope();
+  NanReturnValue(NanNew("C++ GetFragmentSize"));
+}
+
+
