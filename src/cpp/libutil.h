41a2a0ea74121eb31f8b82a612fd9e6f27f43d17
Shafiul tareq <shafiul.hasan@widespace.com>
2015-05-02 13:58:47 +0600

    Libutil class reafactored and methods are implemented

--- src/cpp/libutil.h
+++ src/cpp/libutil.h
@@ -1,10 +1,6 @@
-#include <node.h>
-#include <nan.h>
 #include <erasurecode.h>
 #include <erasurecode_helpers.h>
 
 
-struct ec_args *create_ec_args(ec_backend_id_t be, ec_checksum_type_t ct, int backend_test_idx);
-struct ec_backend_id_t get_ec_backend_id(int id);
-struct ec_checksum_type_t get_ec_checksum_type(int ct);
+ec_backend_id_t get_ec_backend_id(int id);
 
