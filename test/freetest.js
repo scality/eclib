c8bada530260a000e7a3b6a5b703cd37f2e90285
Conrad KLEINESPEL <conradk@conradk.com>
2015-07-09 10:49:03 +0200

    fixes compilation errors

--- test/freetest.js
+++ test/freetest.js
@@ -9,63 +9,62 @@ var hexdump = require('hexdump-nodejs');
 
 console.log("ECLib testing");
 
-var done = false;
-
-function set_done_true() {
-    done = true;
-}
-
 function decode_result(status, out_data, out_data_length) {
-    console.log("Decode Done status=" + status + " data_length=" + out_data_length);
-    
-    //console.log(hexdump(out_data));
+  console.log("Decode Done status=" + status + " data_length=" +
+    out_data_length);
 
-    console.log("comparing");
-    if (buffertools.compare(out_data, ref_buf) == 0)
-	console.log("OK Buffers are identical");
-    else
-	console.log("Nok buffers differ");
+  //console.log(hexdump(out_data));
 
-    setTimeout(set_done_true, 500);
+  console.log("comparing");
+  if (buffertools.compare(out_data, ref_buf) == 0)
+    console.log("OK Buffers are identical");
+  else
+    console.log("Nok buffers differ");
+
+  delete ref_buf;
+  eclib.destroy();
+  global.gc(); //requires --expose-gc
 }
 
-function encode_result(status, encoded_data, encoded_parity, encoded_fragment_length) {
-    console.log("Encode Done status=" + status + " fragment_length=" + encoded_fragment_length);
-    
-    k = eclib.opt.k;
-    m = eclib.opt.m;
-    
-    x = k-1; //available data fragments
-    y = m;   //available parity fragments
-    
-    var fragments = [];
-    var i, j;
-    j = 0;
-    //console.log('data:');
-    for (i = 0;i < x;i++) {
-	//console.log(hexdump(encoded_data[i]));
-	fragments[j++] = encoded_data[i];
-    }
-    //console.log('codings:');
-    for (i = 0;i < y;i++) {
-	//console.log(hexdump(encoded_parity[i]));
-	fragments[j++] = encoded_parity[i];
-    }
-    
-    console.log("decode");
-    eclib.decode(fragments, x+y, encoded_fragment_length, 0, decode_result);
-    console.log("decode started in bg");
+function encode_result(status, encoded_data, encoded_parity,
+  encoded_fragment_length) {
+  console.log("Encode Done status=" + status + " fragment_length=" +
+    encoded_fragment_length);
+
+  k = eclib.opt.k;
+  m = eclib.opt.m;
+
+  x = k - 1; //available data fragments
+  y = m; //available parity fragments
+
+  var fragments = [];
+  var i, j;
+  j = 0;
+  //console.log('data:');
+  for (i = 0; i < x; i++) {
+    //console.log(hexdump(encoded_data[i]));
+    fragments[j++] = encoded_data[i];
+  }
+  //console.log('codings:');
+  for (i = 0; i < y; i++) {
+    //console.log(hexdump(encoded_parity[i]));
+    fragments[j++] = encoded_parity[i];
+  }
+
+  console.log("decode");
+  eclib.decode(fragments, x + y, encoded_fragment_length, 0, decode_result);
+  console.log("decode started in bg");
 }
 
 function test_one() {
 
-    //ref_buf = new Buffer(10000);
-    //buffertools.fill(ref_buf, 'z');
-    //console.log(hexdump(ref_buf));
+  //ref_buf = new Buffer(10000);
+  //buffertools.fill(ref_buf, 'z');
+  //console.log(hexdump(ref_buf));
 
-    console.log("encode");
-    eclib.encode(ref_buf, encode_result);
-    console.log("encode started in bg");
+  console.log("encode");
+  eclib.encode(ref_buf, encode_result);
+  console.log("encode started in bg");
 }
 
 //EC_BACKEND_NULL
@@ -76,10 +75,10 @@ function test_one() {
 //EC_BACKEND_SHSS
 
 var eclib = new ECLib({
-    "bc_id": enums.BackendId["EC_BACKEND_FLAT_XOR_HD"],
-    "k": 3,
-    "m": 3,
-    "hd": 3
+  "bc_id": enums.BackendId["EC_BACKEND_FLAT_XOR_HD"],
+  "k": 3,
+  "m": 3,
+  "hd": 3
 });
 
 eclib.init()
@@ -91,17 +90,3 @@ eclib.init()
 ref_buf = crypto.randomBytes(100000);
 
 test_one();
-
-var i = 0;
-var work = function(dosomestuff) {
-    console.log("dosomestuff in JS "+ (i++));
-    if (!done) {
-        process.nextTick(work);
-    } else {
-	console.log("DONE");
-	delete ref_buf;
-	eclib.destroy();
-	global.gc(); //requires --expose-gc
-    }
-}
-work();
--- test/functest.js
+++ test/functest.js
@@ -10,54 +10,56 @@ var hexdump = require('hexdump-nodejs');
 console.log("ECLib testing");
 
 function test_one(name, opts) {
-    console.log("create " + name);
+  console.log("create " + name);
 
-    var eclib = new ECLib(opts);
-    
-    eclib.init();
-    
-    var ref_buf = crypto.randomBytes(10000000);
+  var eclib = new ECLib(opts);
 
-    eclib.encode(ref_buf,
-		 function(status, encoded_data, encoded_parity, encoded_fragment_length) {
-		     console.log(name + " Encode Done status=" + status + " fragment_length=" + encoded_fragment_length);
-		     
-		     k = eclib.opt.k;
-		     m = eclib.opt.m;
-		     
-		     x = k-1; //available data fragments
-		     y = m;   //available parity fragments
-		     
-		     var fragments = [];
-		     var i, j;
-		     j = 0;
-		     //console.log('data:');
-		     for (i = 0;i < x;i++) {
-			 //console.log(hexdump(encoded_data[i]));
-			 fragments[j++] = encoded_data[i];
-		     }
-		     //console.log('codings:');
-		     for (i = 0;i < y;i++) {
-			 //console.log(hexdump(encoded_parity[i]));
-			 fragments[j++] = encoded_parity[i];
-		     }
-		     
-		     eclib.decode(fragments, x+y, encoded_fragment_length, 0,
-				  function(status, out_data, out_data_length) {
-				      console.log(name + " Decode Done status=" + status + " data_length=" + out_data_length);
+  eclib.init();
 
-				      //console.log(hexdump(out_data));
-				      
-				      if (buffertools.compare(out_data, ref_buf) == 0)
-					  console.log(name + " OK Buffers are identical");
-				      else
-					  console.log(name + " Nok buffers differ");
-				      eclib.destroy();
-				      delete ref_buf;
-				  }
-				 );
-		 }
-		);
+  var ref_buf = crypto.randomBytes(10000000);
+
+  eclib.encode(ref_buf,
+    function(status, encoded_data, encoded_parity, encoded_fragment_length) {
+      console.log(name + " Encode Done status=" + status +
+        " fragment_length=" + encoded_fragment_length);
+
+      k = eclib.opt.k;
+      m = eclib.opt.m;
+
+      x = k - 1; //available data fragments
+      y = m; //available parity fragments
+
+      var fragments = [];
+      var i, j;
+      j = 0;
+      //console.log('data:');
+      for (i = 0; i < x; i++) {
+        //console.log(hexdump(encoded_data[i]));
+        fragments[j++] = encoded_data[i];
+      }
+      //console.log('codings:');
+      for (i = 0; i < y; i++) {
+        //console.log(hexdump(encoded_parity[i]));
+        fragments[j++] = encoded_parity[i];
+      }
+
+      eclib.decode(fragments, x + y, encoded_fragment_length, 0,
+        function(status, out_data, out_data_length) {
+          console.log(name + " Decode Done status=" + status +
+            " data_length=" + out_data_length);
+
+          //console.log(hexdump(out_data));
+
+          if (buffertools.compare(out_data, ref_buf) == 0)
+            console.log(name + " OK Buffers are identical");
+          else
+            console.log(name + " Nok buffers differ");
+          eclib.destroy();
+          delete ref_buf;
+        }
+      );
+    }
+  );
 }
 
 //console.log(hexdump(ref_buf));
@@ -71,15 +73,68 @@ function test_one(name, opts) {
 //EC_BACKEND_ISA_L_RS_CAUCHY
 
 //test_one("null", {"bc_id": enums.BackendId["EC_BACKEND_NULL"], "k": 8, "m": 4});
-test_one("xor", {"bc_id": enums.BackendId["EC_BACKEND_FLAT_XOR_HD"], "k": 3, "m": 3, "hd": 3});
-test_one("vand", {"bc_id": enums.BackendId["EC_BACKEND_JERASURE_RS_VAND"], "k": 10, "m": 4, "w": 16, "hd": 5});
-test_one("vand_44", {"bc_id": enums.BackendId["EC_BACKEND_JERASURE_RS_VAND"], "k": 4, "m": 4, "w": 16, "hd": 5});
-test_one("vand_48", {"bc_id": enums.BackendId["EC_BACKEND_JERASURE_RS_VAND"], "k": 4, "m": 8, "w": 16, "hd": 9});
-test_one("vand_1010", {"bc_id": enums.BackendId["EC_BACKEND_JERASURE_RS_VAND"], "k": 10, "m": 10, "w": 16, "hd": 11});
-test_one("cauchy", {"bc_id": enums.BackendId["EC_BACKEND_JERASURE_RS_CAUCHY"], "k": 10, "m": 4, "w": 4, "hd": 5});
-test_one("cauchy_44", {"bc_id": enums.BackendId["EC_BACKEND_JERASURE_RS_CAUCHY"], "k": 4, "m": 4, "w": 4, "hd": 5});
-test_one("cauchy_48", {"bc_id": enums.BackendId["EC_BACKEND_JERASURE_RS_CAUCHY"], "k": 4, "m": 8, "w": 8, "hd": 9});
-test_one("cauchy_1010", {"bc_id": enums.BackendId["EC_BACKEND_JERASURE_RS_CAUCHY"], "k": 10, "m": 10, "w": 8, "hd": 11});
-test_one("isa_l", {"bc_id": enums.BackendId["EC_BACKEND_ISA_L_RS_VAND"], "k": 10, "m": 4, "w": 8, "hd": 5});
-test_one("isa_l_44", {"bc_id": enums.BackendId["EC_BACKEND_ISA_L_RS_VAND"], "k": 4, "m": 4, "w": 8, "hd": 5});
-test_one("isa_l_1010", {"bc_id": enums.BackendId["EC_BACKEND_ISA_L_RS_VAND"], "k": 10, "m": 10, "w": 8, "hd": 11});
+test_one("xor", {
+  "bc_id": enums.BackendId["EC_BACKEND_FLAT_XOR_HD"],
+  "k": 3,
+  "m": 3,
+  "hd": 3
+});
+test_one("vand", {
+  "bc_id": enums.BackendId["EC_BACKEND_JERASURE_RS_VAND"],
+  "k": 10,
+  "m": 4,
+  "w": 16,
+  "hd": 5
+});
+test_one("vand_44", {
+  "bc_id": enums.BackendId["EC_BACKEND_JERASURE_RS_VAND"],
+  "k": 4,
+  "m": 4,
+  "w": 16,
+  "hd": 5
+});
+test_one("vand_48", {
+  "bc_id": enums.BackendId["EC_BACKEND_JERASURE_RS_VAND"],
+  "k": 4,
+  "m": 8,
+  "w": 16,
+  "hd": 9
+});
+test_one("vand_1010", {
+  "bc_id": enums.BackendId["EC_BACKEND_JERASURE_RS_VAND"],
+  "k": 10,
+  "m": 10,
+  "w": 16,
+  "hd": 11
+});
+test_one("cauchy", {
+  "bc_id": enums.BackendId["EC_BACKEND_JERASURE_RS_CAUCHY"],
+  "k": 10,
+  "m": 4,
+  "w": 4,
+  "hd": 5
+});
+test_one("cauchy_44", {
+  "bc_id": enums.BackendId["EC_BACKEND_JERASURE_RS_CAUCHY"],
+  "k": 4,
+  "m": 4,
+  "w": 4,
+  "hd": 5
+});
+test_one("cauchy_48", {
+  "bc_id": enums.BackendId["EC_BACKEND_JERASURE_RS_CAUCHY"],
+  "k": 4,
+  "m": 8,
+  "w": 8,
+  "hd": 9
+});
+test_one("cauchy_1010", {
+  "bc_id": enums.BackendId["EC_BACKEND_JERASURE_RS_CAUCHY"],
+  "k": 10,
+  "m": 10,
+  "w": 8,
+  "hd": 11
+});
+// test_one("isa_l", {"bc_id": enums.BackendId["EC_BACKEND_ISA_L_RS_VAND"], "k": 10, "m": 4, "w": 8, "hd": 5});
+// test_one("isa_l_44", {"bc_id": enums.BackendId["EC_BACKEND_ISA_L_RS_VAND"], "k": 4, "m": 4, "w": 8, "hd": 5});
+// test_one("isa_l_1010", {"bc_id": enums.BackendId["EC_BACKEND_ISA_L_RS_VAND"], "k": 10, "m": 10, "w": 8, "hd": 11});
