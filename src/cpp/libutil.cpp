824e8d4d115901ff331b8a518b37dc75ce29e829
Lam Pham-Sy <lam.pham-sy@scality.com>
2016-09-06 17:48:54 +0200

    Default field should not be in cases
    Updated for checksumType and also backend ID

--- src/cpp/libutil.cpp
+++ src/cpp/libutil.cpp
@@ -41,8 +41,6 @@ ec_args             *el_create_ec_args(int k, int m, int w, int hd,
 
 ec_backend_id_t     get_ec_backend_id(int id) {
     switch(id) {
-        case 0:
-            return EC_BACKEND_NULL;
         case 1:
             return EC_BACKEND_JERASURE_RS_VAND;
         case 2:
@@ -63,8 +61,6 @@ ec_backend_id_t     get_ec_backend_id(int id) {
 
 ec_checksum_type_t  get_ec_checksum_type(int ct) {
     switch(ct) {
-        case 1:
-            return CHKSUM_NONE;
         case 2:
             return CHKSUM_CRC32;
         case 3:
